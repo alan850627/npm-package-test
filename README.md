80422f7a71d922675ee16919dcd4911c39d0914f
Alan Chen <alchen@silabs.com>
2017-06-15 15:30:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#npm-package-test
+
+simply to test npm package distribution and versioning.
+
+A simple calculator.
---
+++ app.js
@@ -0,0 +1,30 @@
+const commander = require('commander');
+
+const add = require('./lib/commands/add')();
+const multiply = require('./lib/commands/multiply')();
+const average = require('./lib/commands/setupNetwork')();
+
+commander
+  .command(`add [numbers...]`)
+  .description(`Adds the numbers`)
+  .action(add);
+
+commander
+  .command(`mul [numbers...]`)
+  .description(`multiply the numbers`)
+  .action(multiply);
+
+commander
+  .command(`avg [numbers...]`)
+  .description(`calculates the average`)
+  .action(average);
+
+commander.parse(process.argv);
+
+if (noArgs()) {
+  commander.help();
+}
+
+function noArgs() {
+  return !process.argv.slice(2).length;
+}
